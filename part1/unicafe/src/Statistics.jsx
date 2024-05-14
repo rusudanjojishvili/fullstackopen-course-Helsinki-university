b4e38f33226c853c08dd68cbc15f084bbe1a3a08
rusudan jojishvili <rosjojishvili@gmail.com>
2024-05-14 20:20:05 +0400

    exercises 1.6-1.10 with spread props

--- part1/unicafe/src/Statistics.jsx
+++ part1/unicafe/src/Statistics.jsx
@@ -3,9 +3,10 @@
 import StatisticLine from "./StatisticLine"
 
 const Statistics = ({ feedbacks }) => {
-const all = feedbacks.good + feedbacks.neutral + feedbacks.bad
-const average = (feedbacks.good * 1 + feedbacks.bad * (-1))/all
-const positive = feedbacks.good * 100/all
+    const { good, neutral, bad } = feedbacks
+    const all = good + neutral + bad
+    const average = (good * 1 + bad * (-1))/all
+    const positive = good * 100/all
 
   return (
     <div>
@@ -13,9 +14,9 @@ const positive = feedbacks.good * 100/all
         {all === 0? 
         <h3>No feedback given</h3>:
         <div>
-            <StatisticLine text={"good"} value={feedbacks.good} />
-            <StatisticLine text={"neutral"} value={feedbacks.neutral} />
-            <StatisticLine text={"bad"} value={feedbacks.bad} />
+            <StatisticLine text={"good"} value={good} />
+            <StatisticLine text={"neutral"} value={neutral} />
+            <StatisticLine text={"bad"} value={bad} />
             <StatisticLine text={"all"} value={all} />
             <StatisticLine text={"average"} value={average} />
             <StatisticLine text={"positive"} value={`${positive} %`} />
